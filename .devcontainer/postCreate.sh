60b1ed81314b360ac5ac87ce1a114bd281fd274a
Elias Paknejad <627105+elpadev@users.noreply.github.com>
2025-08-16 00:34:45 +0200

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -9,5 +9,6 @@ set -euo pipefail
 minikube start --driver=docker
 
 # Install ArgoCD
+export ARGOCD_VERSION=v3.1.0
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/$ARGOCD_VERSION/manifests/core-install.yaml
