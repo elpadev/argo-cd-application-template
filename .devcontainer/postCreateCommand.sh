bb20107708c9507debadf6653b506d7401ac8398
Elias Paknejad <ali.pjd@gmail.com>
2025-08-15 21:39:30 +0000

    Install ArgoCD into the cluster

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script is executed after the container is created.
+
+# Start minikube
+minikube start --driver=docker
+
+# Install ArgoCD
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
